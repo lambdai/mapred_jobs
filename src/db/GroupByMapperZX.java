371a921ba25c5d4a34c263ae566108d286a57ef1
XiaZhang <xiazhang86@gmail.com>
2014-05-21 15:52:14 -0700

    Create GroupByMapperZX.java

---
+++ src/db/GroupByMapperZX.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import db.table.*;
+
+public class GroupByMapperV1ZX extends Mapper<BytesWritable,BytesWritable,BytesWritable,BytesWritable>{
+	int[] keyColIndxs;
+	int[] valColIndxs;	// indexes for key and values in output
+	Row row;	//	one row
+	BytesWritable tKey;	
+	BytesWritable tVal;	// key and values in BytesWritable
+	
+	@Override
+	public void setup(Context cont) throws IOException, InterruptedException{
+		Configuration conf= cont.getConfiguration();
+		String gb_key_str=conf.get("Constant.AGG_COLUMN_ID"); // guessing these are keys for group by
+		String schema_str=conf.get("group by schema"); // get the schema
+		Schema schema = new Schema("whatever the schema is"); // schema for the row
+		schema.parseAndSetRecordDescriptor(schema_str);
+		row=Row.createBySchema(schema);// create the row
+		keyColIndxs=Schema.columnIndexes(schema, SchemaUtils.parseColumns(gb_key_str));	
+		valColIndxs=SchemaUtils.columnLeft(keyColIndxs, schema.getRecordDescriptor().size());	//get the column indexes of the group by keys and the values
+		tKey=new BytesWritable();
+		tVal=new BytesWritable();
+	}
+	
+	@Override
+	public void map(BytesWritable key, BytesWritable value, Context cont) throws IOException, InterruptedException{
+		row.readFieldsFromBytes(value);
+		row.writeToBytes(tKey, keyColIndxs);
+		row.writeToBytes(tVal, valColIndxs);
+		cont.write(tKey,tVal);	
+	}
+}
+	
