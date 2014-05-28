48f9cf34ed049ebe67928cc2b1e5eaaf4f4ea3f2
XiaZhang <xiazhang86@gmail.com>
2014-05-27 22:22:00 -0700

    Create Selector.java

---
+++ Selector.java
@@ -0,0 +1,28 @@
+package db;
+import db.table.Field;
+import db.table.Row;
+import db.table.SchemaUtils;
+import db.table.Schema;
+
+public class Selector {
+	private int[] colIndexes;
+	private Row outRow;
+	private Schema inSchema;
+	private Schema outSchema;
+	
+	public Selector(Schema schema, String columnNames){
+		inSchema=schema;
+		outSchema = new Schema("SELECT FILTER");
+		outSchema.parseAndSetRecordDescriptor(columnNames);
+		colIndexes = Schema.columnIndexes(inSchema, SchemaUtils.parseColumns(columnNames));
+		outSchema = inSchema.createSubSchema(colIndexes);		
+	}
+	
+	//set the fields in output row and spit the row out 
+	public Row select(Row inRow){
+		for(int i=0;i<colIndexes.length;i++){
+			outRow.setField(inRow.getFields()[colIndexes[i]], i);
+		}		
+		return outRow;
+	}
+}
